8e8a8d08a279feeae623d2c0217a19a239201b33
Administrator <Administrator@WIN-NR57M63DD8T>
2021-06-16 20:37:09 +0200

    Fix include, so it'll compile.

--- src/process/hooks/example_hook.h
+++ src/process/hooks/example_hook.h
@@ -2,11 +2,12 @@
 #define INTERNAL_TEMPLATE_EXAMPLE_HOOK_H
 
 #include <cstdint>
+#include "../../utils/pattern_scan.h"
 
 namespace process::hooks::example_hook {
     uint32_t get_address() {
         // return address;
-        return pattern_scan("module_name.dll", "AA ?? BB ?? CC ?? DD ?? EE ?? FF");
+        return utils::pattern_scan("module_name.dll", "AA ?? BB ?? CC ?? DD ?? EE ?? FF");
     }
 
     using type = void (__cdecl *)(void *);
